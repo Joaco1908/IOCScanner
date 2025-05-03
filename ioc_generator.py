f7b73093cc7cc503314b6fe0f7ced2818e938e55
nmoscatelli <nicolasmoscatelli1708@gmail.com>
2025-05-03 13:26:56 +0200

    added flags to ioc_generator.py

--- ioc_generator.py
+++ ioc_generator.py
@@ -1,33 +1,17 @@
-import re
-import json
-import ipaddress
+from rich import print
+import argparse 
+
+# It will create a JSON file to scan it with ioc_scanner
+# Two flags: one for the output filename -o and another one to add more information to a .json file -a 
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate and manipulate JSON files")
+
+    parser.add_argument('-o', '--output', type=str, help="Name of the output file", required=True)
+    parser.add_argument('-a', '--append', type=str, help="Modify a json file and append information")
+
+    args = parser.parse_args()
 
 
-# --- Validation helpers ---
-
-
-def is_valid_ip(ip):
-    try:
-        ipaddress.ip_address(ip)
-        return True
-    except ValueError:
-        return False
-    
-def is_valid_email(email):
-    pattern = r'^[a-zA-Z0-9._+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-    return re.match(pattern, email) is not None
-
-def is_valid_hash(h):
-    h = h.lower()
-    if len(h) == 32 and re.fullmatch(r"[a-f0-9]{32}",h): #MD5
-        return True
-    if len(h) == 40 and re.fullmatch(r"[a-f0-9]{40}",h): #SHA1
-        return True
-    if len(h) == 64 and re.fullmatch(r"[a-f0-9]{64}",h): #SHA256
-        return True
-    return False
-
-# --- Main script logic ---
-
-def Main():
-    
+if __name__ == "__main__":
+    main()
